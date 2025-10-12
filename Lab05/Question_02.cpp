ade67564311b20beea8acc338d636c3d52e6be35
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-10-12 20:35:12 +0500

    Update Question_02.cpp

--- Lab05/Question_02.cpp
+++ Lab05/Question_02.cpp
@@ -1,1 +1,121 @@
+#include <iostream>
+using namespace std;
 
+/*
+  Syed Wahaaj Ali
+  CT-24035
+*/
+
+class deque {
+	private:
+		int *arr;
+		int front;
+		int rear;
+		int size;
+		int capacity;
+
+	public:
+		deque(int n) {
+			arr = new int[n];
+			front = 0;
+			rear = -1;
+			size = 0;
+			capacity = n;
+		}
+
+		bool isEmpty() {
+			return size == 0;
+		}
+
+		bool isFull() {
+			return size == capacity;
+		}
+
+		void insertRear(int x) {
+			if (isFull()) return;
+			rear = (rear + 1) % capacity;
+			arr[rear] = x;
+			size++;
+		}
+
+		void insertFront(int x) {
+			if (isFull()) return;
+			front = (front - 1 + capacity) % capacity;
+			arr[front] = x;
+			size++;
+		}
+
+		void deleteFront() {
+			if (isEmpty()) return;
+			front = (front + 1) % capacity;
+			size--;
+		}
+
+		void deleteRear() {
+			if (isEmpty()) return;
+			rear = (rear - 1 + capacity) % capacity;
+			size--;
+		}
+
+		int getFront() {
+			if (isEmpty()) return -1;
+			return arr[front];
+		}
+
+		int getRear() {
+			if (isEmpty()) return -1;
+			return arr[rear];
+		}
+
+		int getSize() {
+			return size;
+		}
+
+		~deque() {
+			delete[] arr;
+		}
+};
+
+int findWinner(int n, int k) {
+	deque dq(n);
+
+	// put all friends in deque
+	for (int i = 1; i <= n; i++) {
+		dq.insertRear(i);
+	}
+
+	while (dq.getSize() > 1) {
+		// move k-1 friends from front to rear
+		for (int i = 0; i < k - 1; i++) {
+			int temp = dq.getFront();
+			dq.deleteFront();
+			dq.insertRear(temp);
+		}
+		// remove kth friend
+		dq.deleteFront();
+	}
+
+	return dq.getFront();  // last friend left
+}
+
+int main() {
+	int n, k;
+
+	cout << "Enter the number of friends (n): ";
+	cin >> n;
+
+	cout << "Enter the counting number (k): ";
+	cin >> k;
+
+	while (k > n) {
+		cout << "\nInvalid! k can't be greater than n." << endl;
+		cout << "Enter valid k: ";
+		cin >> k;
+	}
+
+	int winner = findWinner(n, k);
+
+	cout << "\nWinner is Friend no. " << winner << endl;
+
+	return 0;
+}
